f6777e4411632e78dd7fa3c79a9b46acfa68db3c
Tlehel <tulit.lehel@student.ms.sapientia.ro>
2025-11-25 09:00:29 +0200

    k

--- lab10/SettlementStatistics.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by Lehel on 2025-11-25.
-//
-
-#include "SettlementStatistics.h"
--- lab10/SettlementStatisticsImpl.h
+++ lab10/SettlementStatisticsImpl.h
@@ -4,8 +4,23 @@
 
 #ifndef SETTLEMENTSTATISTICSIMPL_H
 #define SETTLEMENTSTATISTICSIMPL_H
+#include "Settlement.h"
+#include <vector>
+#include <string>
 
-
+struct SettlementStatistics {
+    virtual int numSettlements() const = 0;
+    virtual int numCounties() const = 0;
+    virtual int numSettlementsByCounty(const std::string& county) const = 0;
+    virtual std::vector<Settlement> findSettlementsByCounty(
+    const std::string& county) const = 0;
+    virtual Settlement findSettlementsByNameAndCounty(
+    const std::string& name, const std::string& county) const = 0;
+    virtual Settlement maxPopulationBySettlement() const = 0;
+    virtual Settlement minPopulationBySettlement() const = 0;
+    virtual std::vector<Settlement> findSettlementsByName(
+    const std::string& name) = 0;
+};
 
 class SettlementStatisticsImpl {
 
